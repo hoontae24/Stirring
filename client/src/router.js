7c1039cda7656847de27dba0e7880b45385bfa5c
Mounc <kadr8941@naver.com>
2019-01-20 21:53:01 +0900

    :04 Resetting Client side using vue, vuex, vue-router, vuetify.

--- client/src/router.js
+++ client/src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -13,11 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      components: {
-        header: Header,
-        default: Home,
-        footer: Footer
-      }
+      component: Home
     },
     {
       path: '/about',
@@ -26,15 +20,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path:'/login',
-      name: 'login',
-      components: {
-        header: Header,
-        default: Home,
-        footer: Footer
-      }
     }
   ]
 })
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="home">
-    msg : {{ msg }}
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'home',
-  data: ()=>({
-    
-  })
-}
+  export default {
+    components: {
+      HelloWorld
+    }
+  }
 </script>
