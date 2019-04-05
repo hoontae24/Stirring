aebff615ead8328023001980d99772c15d7e4ff4
Mounc <kadr8941@naver.com>
2019-04-05 19:58:51 +0900

    :28 Refactoring API router

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,16 @@
+
 const express = require('express')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.send('This API is working.')
+})
 
 const auth = require('./auth')
 const users = require('./users')
 const posts = require('./posts')
 const collections = require('./collections')
 const tag = require('./tag')
-
-router.get('/', (req, res) => {
-  res.send('This API is working.')
-})
 
 router.use('/auth', auth)
 router.use('/users', users)
@@ -18,3 +19,4 @@ router.use('/collections', collections)
 router.use('/tag', tag)
 
 module.exports = router
+
