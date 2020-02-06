b628cf4525a47ce1ae4b5fecf4adf45f952cffe9
hoontae24 <kadr8941@naver.com>
2020-02-07 00:56:55 +0900

    Ci: Initialize DB loader

--- api/src/loaders/db.ts
+++ api/src/loaders/db.ts
@@ -1,7 +1,20 @@
+import mongoose from 'mongoose';
+import { env } from 'consts';
+
 const loader: AppInitializer = async ({ app }) => {
   // Initialize and Connect Database
 
-  console.log('Connect DataBase');
+  const uri = `mongodb://${env.DATABASE_HOST}:${env.DATABASE_PORT}/${env.DATABASE_NAME}`;
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  mongoose.set('useCreateIndex', true);
+  const db = mongoose.connection;
+  db.on('error', console.error);
+  db.once('open', () => {
+    console.log('Connected to mongoDB server.\n');
+  });
 
   return { app };
 };
