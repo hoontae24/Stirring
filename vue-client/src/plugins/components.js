9a14907a3e9c12e8babdcf8a394be3f67c34abab
hoontae24 <kadr8941@naver.com>
2020-04-12 19:19:08 +0900

    Ci: Add auto registering global components

--- vue-client/src/plugins/components.js
+++ vue-client/src/plugins/components.js
@@ -1,10 +1,13 @@
-import VueLink from '@/components/_globals/VueLink';
-import VueInput from '@/components/_globals/VueInput';
-import VueButton from '@/components/_globals/VueButton';
-import VueDivider from '@/components/_globals/VueDivider';
-
-const parts = [VueLink, VueInput, VueButton, VueDivider];
-
 export default ({ app }) => {
-  parts.forEach(part => app.component(part.name, part));
+  const requireComponent = require.context(
+    '@/components/_globals/',
+    false,
+    /(\w+).vue$/,
+  );
+
+  requireComponent.keys().forEach(fileName => {
+    const { default: component } = requireComponent(fileName) || {};
+    if (!component) return;
+    app.component(component.name, component);
+  });
 };
